f08ad28b1a876cb4aea3c7868bf3363d4de750f0
Yifdaddy <ivan.he901@gmail.com>
2018-11-28 21:59:28 -0600

    Update viewmenu.php
    Parsed JSON line by line

--- viewmenu.php
+++ viewmenu.php
@@ -24,11 +24,23 @@
 
   $sql = "SELECT menu FROM menudata WHERE user_id=".$_SESSION['u_id'];
   $result = $conn->query($sql);
-
+  //echo ($result);
+  //echo json_decode($result);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["menu"] . "<br>";
+      //echo $row["menu"] . "<br>";
+      $result_decode = json_decode($row["menu"], true);
+      //$test = '[{"name": "Burger", "price": "$6.99"}, {"name": "Egg","price": "$3.99"}]';
+      //echo $test;
+      //$test_decode = json_decode($test, true);
+      //print_r($res); Array
+      //var_dump($res);
+      //$character = json_decode($row["menu"]);
+      //echo $character->name;
+      foreach ($result_decode as $key => $value) {
+        echo $value["name"] . "," . $value["price"] . "<br>";
+      }
     }
 } else {
     echo "0 results";
