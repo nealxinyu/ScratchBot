e487d8c02fbad868952375bdbb67cf669c502633
Jianwei Hu <jianwei-hu@uiowa.edu>
2018-12-02 17:03:59 -0600

    home assets images

---
+++ home.php
@@ -0,0 +1,157 @@
+<?php include 'header.php';?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>ScratchBot Home</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body class="landing">
+
+<!-- Banner -->
+<section id="banner">
+    <h2>ScratchBot</h2>
+    <p>Next generation chat-bot generator <br /> For resaurant owner!</p>
+    <ul class="actions">
+        <li><a href="#" class="button special big">Get Started</a></li>
+    </ul>
+</section>
+
+<!-- Intuitive and Powerful -->
+<section class="wrapper style1">
+    <div class="container 75%">
+        <div class="row 200%">
+            <div class="6u 12u$(medium)">
+                <header class="major">
+                    <h2>Intuitive and Powerful</h2>
+                    <p>Design for the people who has no programming background</p>
+                </header>
+            </div>
+            <div class="6u$ 12u$(medium)">
+                <p>
+                    Powered by Google Blockly, the user can use our website to build a unique scratchBot
+                    suits their
+                    restaurant service easily. User can focus on the function they want on the scratchBot
+                    and worries
+                    nothing about how the code is accomplished.
+                </p>
+                <p>
+                    The website supports comprehensive functions to the scratchBot. Personalized greeting, personalize
+                    menu, take orders and etc.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container 75%">
+        <div class="row 200%">
+            <div class="6u 12u$(medium)">
+                <p>
+                    scratchBot integrated database that stores user information for the restaurant customer. Once setup,
+                    user won't need to input any other information.
+                </p>
+                <p>
+                    scratchBot supports user login, make it possible to be used in any machine.
+                </p>
+            </div>
+            <div class="6u$ 12u$(medium)">
+                <header class="major">
+                    <h2>Integrated and Portable</h2>
+                    <p>Design to be used anywhere.</p>
+                </header>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="wrapper style2">
+    <div class="container">
+        <header class="major special">
+            <h2>Features</h2>
+        </header>
+        <div class="feature-grid">
+            <div class="feature">
+                <div class="image rounded"><img src="images/pic04.jpg" alt="" /></div>
+                <div class="content">
+                    <header>
+                        <h4>Login/Signup</h4>
+                        <p>build scratchBot anywhere</p>
+                    </header>
+                    <p>User can login and store scratchBot in their own account and can work on them later.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="image rounded"><img src="images/pic05.jpg" alt="" /></div>
+                <div class="content">
+                    <header>
+                        <h4>drag-and-design</h4>
+                        <p>Intuitive way without coding background</p>
+                    </header>
+                    <p>personalized greeting, personalize menu, take orders.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="image rounded"><img src="images/pic06.jpg" alt="" /></div>
+                <div class="content">
+                    <header>
+                        <h4>Online scratchBot Service</h4>
+                        <p>For the customer</p>
+                    </header>
+                    <p>User can upload the scratchBot online. The scratchBot will provide service independently</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="image rounded"><img src="images/pic07.jpg" alt="" /></div>
+                <div class="content">
+                    <header>
+                        <h4>Share your scratchBot</h4>
+                        <p>For the benefits of the community</p>
+                    </header>
+                    <p>Share your scratchBot to the community. Other scratchBot user can build theirs base on your design</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="wrapper style1 special">
+    <div class="container">
+        <header class="major">
+            <h2>Questions about us or the scratchBot?</h2>
+        </header>
+        <div class="row 150%">
+            <div class="6u 12u$(xsmall)">
+                <a href="about.php">
+                    <div class="image fit captioned">
+                        <img src="images/pic02.jpg" alt="" />
+                        <h3>Meet the ScratchBot Team!</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="6u$ 12u$(xsmall)">
+                <a href="help.php">
+                    <div class="image fit captioned">
+                        <img src="images/pic03.jpg" alt="" />
+                        <h3>Read the tutorial!</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
+
+<?php include 'footer.php';?>
