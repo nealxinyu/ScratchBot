8e2844dabe5d52b60541559a4c822bbcb3273078
Yifdaddy <ivan.he901@gmail.com>
2018-12-01 23:01:03 -0600

    Updated chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -5,7 +5,24 @@
 <style>
 	<?php include 'css/menu.css'; ?>
 </style>
-	
+<div class="container">
+  <div class="row">
+    <div class="col-md-3 ">
+      <?php
+      // Display a message when the user logs in
+        if (isset($_SESSION['u_id'])) {
+          echo  "<h2>".($_SESSION['u_uid']."</h2>");
+        }
+      ?>
+         <div class="list-group ">
+              <a href="user.php" class="list-group-item list-group-item-action ">Profile</a> 
+
+              <a href="viewmenu.php" class="list-group-item list-group-item-action ">View Menu</a>
+              <a href="uploadmenu.php" class="list-group-item list-group-item-action">Edit Menu</a>
+              <a href="chatbot.php" class="list-group-item list-group-item-action active">Saved ChatBot</a> 
+            </div> 
+    </div>
+    <div class="col-md-9">
 <?php
 
   $server = "db4free.net";
@@ -37,6 +54,8 @@ if ($result->num_rows > 0) {
 
   $conn->close();
 ?>
+</div>
+
 
 <?php
 	include_once 'footer.php';
