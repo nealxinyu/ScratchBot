50abe4be9fe7a278cd819385d1c08777c8e31d06
Chen, Xinyu <xinyu-chen-1@uiowa.edu>
2018-12-02 16:37:44 -0600

    updates

---
+++ db_get_restaurant.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+if (isset($_SESSION['u_id'])) {
+  $server = "db4free.net";
+  $username = "scratchbot";
+  $password = "qaz123wsx";
+  $dbname = "scratchbot";
+
+  // Create connection
+  $conn = new mysqli($server, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "SELECT `restaurantName`,`restaurantPhone`,`restaurantAddress` FROM `users` WHERE user_id=".$_SESSION['u_id'];
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          echo $row["restaurantName"];
+          echo ",";
+          echo $row["restaurantPhone"];
+          echo ",";
+          echo $row["restaurantAddress"];
+      }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+} else {
+    echo "please login";
+}
+
+
+?>
