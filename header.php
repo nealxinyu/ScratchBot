e91d26149ab1acd6574b7981b632d05a18f84012
魔法少jio <mashirowhite333@gmail.com>
2018-12-01 21:02:34 -0600

    Update header.php

--- header.php
+++ header.php
@@ -4,44 +4,65 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ScratchBot</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Scratch Bot</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1， shrink-to-fit=no">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
-                <a href="index.php" class="navbar-brand">ScratchBot</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="help.php">Help</a></li>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="index.php">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="help.php">Help</a>
+                </li>
             </ul>
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="navbar-nav ml-auto">
                 <?php
-                    //If the user is logged in
-                    if (isset($_SESSION['u_id'])) {
-                        echo '
-                        <li><a href="user.php">'. "Name: " .$_SESSION['u_uid']. ", ". "ID: " . $_SESSION['u_id'].'</a></li>
+                //If the user is logged in
+                if (isset($_SESSION['u_id'])) {
+                    echo '
+                        <li><a href="user.php">'.$_SESSION['u_uid'].'</a></li>
                         <li>
                             <form action="includes/logout.inc.php" method="POST">
                                 <button type="submit" name="submit" class="btn btn-outline-danger" style="margin-top: 8px;">Logout</button>
                             </form>
                         </li>';
-                    }
-                    //If the user is not logged in
-                    else {
-                        echo '
-                        <li><a href="signup.php">Sign up</a></li>
-                        <li><a href="login.php">Login</a></li>
+                }
+                //If the user is not logged in
+                else {
+                    echo '
+                        <li class="nav-item">
+                            <a class="nav-link" href="signup.php">Sign up</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
                         ';
-                    }
+                }
                 ?>
             </ul>
         </div>
     </nav>
+</body>
+</html>
