67103500b13496799131a52ffe56453c2c4bbb59
Jianwei Hu <jianwei-hu@uiowa.edu>
2018-12-02 17:37:57 -0600

    about

--- about.php
+++ about.php
@@ -1,125 +1,214 @@
 <?php include 'header.php';?>
 
-<div class="container">
-	<h1>Team Members</h1>
-	</br></br>
-	<div class="row">
-		<div class="col-sm-4">
-			<!-- Xinyu Chen -------------------------------------------------------------->
-			<img src="avatar.jpg" alt="tiger" height="200" style="border:2px solid black">
-			<h1><strong>Xinyu Chen</strong></h1>
-			<h2><em>Computer Science Student</em></h2>
-			<h3>Skills</h3>
-			<ul>
-				<li>HTML</li>
-				<li>CSS</li>
-				<li>JavaScript</li>
-			</ul>
-			<h3>Hobbies</h3>
-			<ul>
-				<li>Programming</li>
-				<li>Playing Videogames</li>
-				<li>Running</li>
-			</ul>
-			<hr>
-		</div>
-		<div class="col-sm-4">
-			<!-- Fred Morsy -------------------------------------------------------------->
-			<img src="avatar.jpg" alt="panda" height="200" style="border:2px solid black">
-			<h1><strong>Fred Morsy</strong></h1>
-			<h2><em>Computer Science Student</em></h2>
-			<h3>Skills</h3>
-			<ul>
-				<li>HTML</li>
-				<li>CSS</li>
-				<li>JavaScript</li>
-			</ul>
-			<h3>Hobbies</h3>
-			<ul>
-				<li>Programming</li>
-				<li>Playing Videogames</li>
-				<li>Running</li>
-			</ul>
-			<hr>
-		</div>
-		<div class="col-sm-4">
-			<!-- Yifan He -------------------------------------------------------------->
-			<img src="avatar.jpg" alt="red panda" height="200" style="border:2px solid black">
-			<h1><strong>Yifan He</strong></h1>
-			<h2><em>Computer Science Student</em></h2>
-			<h3>Skills</h3>
-			<ul>
-				<li>HTML</li>
-				<li>CSS</li>
-				<li>JavaScript</li>
-			</ul>
-			<h3>Hobbies</h3>
-			<ul>
-				<li>Programming</li>
-				<li>Playing Videogames</li>
-				<li>Running</li>
-			</ul>
-			<hr>
-		</div>
-		<div class="col-sm-4">
-			<!-- Xiaoyu Xing -------------------------------------------------------------->
-			<img src="avatar.jpg" alt="fish" height="200" style="border:2px solid black">
-			<h1><strong>Xiaoyu Xing</strong></h1>
-			<h2><em>Computer Science Student</em></h2>
-			<h3>Skills</h3>
-			<ul>
-				<li>Python</li>
-				<li>Java</li>
-				<li>Data Analytics</li>
-			</ul>
-			<h3>Hobbies</h3>
-			<ul>
-				<li>Photography</li>
-				<li>Playing Videogames</li>
-				<li>Running</li>
-			</ul>
-		</div>
-		<div class="col-sm-4">
-			<!-- Zixu Wang -------------------------------------------------------------->
-			<img src="avatar.jpg" alt="lion" height="200" style="border:2px solid black">
-			<h1><strong>Zixu Wang</strong></h1>
-			<h2><em>Computer Science Student</em></h2>
-			<h3>Skills</h3>
-			<ul>
-				<li>JAVA</li>
-				<li>C#</li>
-				<li>HTML/CSS/JS/XML</li>
-			</ul>
-			<h3>Hobbies</h3>
-			<ul>
-				<li>Programming</li>
-				<li>Playing Videogames</li>
-				<li>Translating</li>
-			</ul>
-		</div>
-		<div class="col-sm-4">
-			<!-- Jianwei Hu -------------------------------------------------------------->
-			<img src="avatar.jpg" alt="cat" height="200" style="border:2px solid black">
-			<h1><strong>Jianwei Hu</strong></h1>
-			<h2><em>Biomedical Engineering Student</em></h2>
-			<h3>Skills</h3>
-			<ul>
-				<li>HTML</li>
-				<li>CSS</li>
-				<li>Python</li>
-			</ul>
-			<h3>Hobbies</h3>
-			<ul>
-				<li>Programming</li>
-				<li>Playing Videogames</li>
-				<li>Swimming</li>
-			</ul>
-		</div>
-	</div>
-	<footer>
-		<hr>
-		<p>Team Scratchbot</p>
-	</footer>
-</div>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>ScratchBot About</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body class="landing">
+
+
+<!-- Banner -->
+<section id="banner">
+    <h2>About us</h2>
+    <p>Meet <br>the University of Iowa <br> Fundamentals of Software Engineering  <br>team Scratchbot!</p>
+</section>
+
+<!-- Intuitive and Powerful -->
+<section class="wrapper style1 special">
+    <div class="container 75%">
+        <div class="row 200%">
+            <p>
+                We are an imaginary and hardworking team consisting of six people with unique backgrounds. In this
+                class, we are building a food ordering website with a Scratch Bot with the target user focused on
+                fast food restaurant owners. In order to suit the user’s requirement, we went through the agile
+                programming life cycle. That means we did sprint programming for 1~2 weeks and demoed our product
+                to the user multiple times to get incremental feedback.
+            </p>
+        </div>
+    </div>
+    <div class="container 75%">
+        <div class="row 150%">
+            <div class="12u 12u$(xsmall)">
+                <div class="image fit captioned">
+                    <img src="images/agile.png" alt="" />
+                    <h3>Agile makes life easier!</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="wrapper style2 special">
+    <div class="container">
+        <header class="major">
+            <h2>Team ScratchBot!</h2>
+        </header>
+
+        <div class="row 150%">
+            <div class="12u 12u$(xsmall)">
+                <div class="image fit captioned">
+                    <img src="images/pic02.jpg" alt="" />
+                    <h3>
+                        Xinyu Chen
+                    </h3>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
+
+<section class="wrapper style1">
+    <div class="container">
+            <div class="feature">
+                <div class="image rounded"><img height="150" src="images/aleksey.jpeg" alt="" /></div>
+                <div class="content">
+                    <header>
+                        <h4>Professor. Aleksey Gurtovoy</h4>
+                    </header>
+                    <p>Here we would like to thank Professor Aleksey Gurtovoy, our instructor. He has been playing the role of the potential customer for the whole semester. We learned a lot from the developer-user interaction simulations. We believe that this whole experience is very useful and will be a valuable asset for our future career.</p>
+                </div>
+            </div>
+</section>
+
+
+
+
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
+
+<!--<div class="container">-->
+<!--	<h1>Team Members</h1>-->
+<!--	</br></br>-->
+<!--	<div class="row">-->
+<!--		<div class="col-sm-4">-->
+<!--			<!-- Xinyu Chen -------------------------------------------------------------->-->
+<!--			<img src="avatar.jpg" alt="tiger" height="200" style="border:2px solid black">-->
+<!--			<h1><strong>Xinyu Chen</strong></h1>-->
+<!--			<h2><em>Computer Science Student</em></h2>-->
+<!--			<h3>Skills</h3>-->
+<!--			<ul>-->
+<!--				<li>HTML</li>-->
+<!--				<li>CSS</li>-->
+<!--				<li>JavaScript</li>-->
+<!--			</ul>-->
+<!--			<h3>Hobbies</h3>-->
+<!--			<ul>-->
+<!--				<li>Programming</li>-->
+<!--				<li>Playing Videogames</li>-->
+<!--				<li>Running</li>-->
+<!--			</ul>-->
+<!--			<hr>-->
+<!--		</div>-->
+<!--		<div class="col-sm-4">-->
+<!--			<!-- Fred Morsy -------------------------------------------------------------->-->
+<!--			<img src="avatar.jpg" alt="panda" height="200" style="border:2px solid black">-->
+<!--			<h1><strong>Fred Morsy</strong></h1>-->
+<!--			<h2><em>Computer Science Student</em></h2>-->
+<!--			<h3>Skills</h3>-->
+<!--			<ul>-->
+<!--				<li>HTML</li>-->
+<!--				<li>CSS</li>-->
+<!--				<li>JavaScript</li>-->
+<!--			</ul>-->
+<!--			<h3>Hobbies</h3>-->
+<!--			<ul>-->
+<!--				<li>Programming</li>-->
+<!--				<li>Playing Videogames</li>-->
+<!--				<li>Running</li>-->
+<!--			</ul>-->
+<!--			<hr>-->
+<!--		</div>-->
+<!--		<div class="col-sm-4">-->
+<!--			<!-- Yifan He -------------------------------------------------------------->-->
+<!--			<img src="avatar.jpg" alt="red panda" height="200" style="border:2px solid black">-->
+<!--			<h1><strong>Yifan He</strong></h1>-->
+<!--			<h2><em>Computer Science Student</em></h2>-->
+<!--			<h3>Skills</h3>-->
+<!--			<ul>-->
+<!--				<li>HTML</li>-->
+<!--				<li>CSS</li>-->
+<!--				<li>JavaScript</li>-->
+<!--			</ul>-->
+<!--			<h3>Hobbies</h3>-->
+<!--			<ul>-->
+<!--				<li>Programming</li>-->
+<!--				<li>Playing Videogames</li>-->
+<!--				<li>Running</li>-->
+<!--			</ul>-->
+<!--			<hr>-->
+<!--		</div>-->
+<!--		<div class="col-sm-4">-->
+<!--			<!-- Xiaoyu Xing -------------------------------------------------------------->-->
+<!--			<img src="avatar.jpg" alt="fish" height="200" style="border:2px solid black">-->
+<!--			<h1><strong>Xiaoyu Xing</strong></h1>-->
+<!--			<h2><em>Computer Science Student</em></h2>-->
+<!--			<h3>Skills</h3>-->
+<!--			<ul>-->
+<!--				<li>Python</li>-->
+<!--				<li>Java</li>-->
+<!--				<li>Data Analytics</li>-->
+<!--			</ul>-->
+<!--			<h3>Hobbies</h3>-->
+<!--			<ul>-->
+<!--				<li>Photography</li>-->
+<!--				<li>Playing Videogames</li>-->
+<!--				<li>Running</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<div class="col-sm-4">-->
+<!--			<!-- Zixu Wang -------------------------------------------------------------->-->
+<!--			<img src="avatar.jpg" alt="lion" height="200" style="border:2px solid black">-->
+<!--			<h1><strong>Zixu Wang</strong></h1>-->
+<!--			<h2><em>Computer Science Student</em></h2>-->
+<!--			<h3>Skills</h3>-->
+<!--			<ul>-->
+<!--				<li>JAVA</li>-->
+<!--				<li>C#</li>-->
+<!--				<li>HTML/CSS/JS/XML</li>-->
+<!--			</ul>-->
+<!--			<h3>Hobbies</h3>-->
+<!--			<ul>-->
+<!--				<li>Programming</li>-->
+<!--				<li>Playing Videogames</li>-->
+<!--				<li>Translating</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<div class="col-sm-4">-->
+<!--			<!-- Jianwei Hu -------------------------------------------------------------->-->
+<!--			<img src="avatar.jpg" alt="cat" height="200" style="border:2px solid black">-->
+<!--			<h1><strong>Jianwei Hu</strong></h1>-->
+<!--			<h2><em>Biomedical Engineering Student</em></h2>-->
+<!--			<h3>Skills</h3>-->
+<!--			<ul>-->
+<!--				<li>HTML</li>-->
+<!--				<li>CSS</li>-->
+<!--				<li>Python</li>-->
+<!--			</ul>-->
+<!--			<h3>Hobbies</h3>-->
+<!--			<ul>-->
+<!--				<li>Programming</li>-->
+<!--				<li>Playing Videogames</li>-->
+<!--				<li>Swimming</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--	</div>-->
+<!--	<footer>-->
+<!--		<hr>-->
+<!--		<p>Team Scratchbot</p>-->
+<!--	</footer>-->
+<!--</div>-->
 
 <?php include 'footer.php';?>
