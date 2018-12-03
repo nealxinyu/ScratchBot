eb6bd06e31b955f359e7aeff2a936b623a410bfe
Jianwei Hu <jianwei-hu@uiowa.edu>
2018-12-02 19:56:33 -0600

    about

--- footer.php
+++ footer.php
@@ -1,2 +1,19 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body>
+<footer id="footer">
+    <div class="container">
+        <ul class="copyright">
+            <li>&copy; ScratchBot</li>
+            <li>Intro to Software Engineering team</li>
+            <li>GitHub: <a href="https://github.com/uiowa-cs-5800-0001-fall-2018/Scratch-Bot">ScratchBot</a></li>
+        </ul>
+    </div>
+</footer>
 </body>
 </html>
+
