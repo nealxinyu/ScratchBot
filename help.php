a176d24eaac862cb4d7f4ea40c97d9760278c54d
Jianwei Hu <jianwei-hu@uiowa.edu>
2018-12-02 19:55:58 -0600

    about

--- help.php
+++ help.php
@@ -1,9 +1,54 @@
 <?php include 'header.php';?>
 
-<div class="container">
-    <h1>
-        Help
-    </h1>
-</div>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>ScratchBot Help</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+    </head>
+    <body class="landing">
+
+    <section id="banner">
+        <h2>Help</h2>
+    </section>
+
+    <section class="wrapper style1 ">
+        <div class="container 75%">
+            <div class="row 200%">
+                <p>The main page consists of three panels:</p>
+            </div>
+        </div>
+        <div class="container 75%">
+            <div class="row 150%">
+                <div class="12u 12u$(xsmall)">
+                    <div class="image fit captioned">
+                        <img src="images/agile.png" alt="" />
+                        <h3></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container 75%">
+            <div class="row 200%">
+                <p>
+                    Toggle the operation you want on the left to the workspace and put in the sequence you like. The resulting scratch bot will appear on your right.
+                </p>
+                <p>
+                    You can save the scratch bot on our server. A web address will be given to you which provides the scratch bot services.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/skel.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
 
 <?php include 'footer.php';?>
